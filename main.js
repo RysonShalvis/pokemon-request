d6239d461200a05a6e206851f020bf976f172ecd
Ryson Shalvis <rysonshalvis@Rysons-MacBook-Pro.local>
2020-09-16 00:21:04 -0600

    add loading element

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
 const button = document.querySelector('button');
 const weight = document.querySelector('#pokemon-weight');
 const pokemonText = document.querySelector('#pokemon-text');
+const loadContainer = document.querySelector('.load-container');
 function pokemonRequest () {
+    
+    let loading = document.createElement('p');
+    loading.innerHTML = 'Loading';
+    loadContainer.appendChild(loading);
     const pokemonName = pokemonText.value;
+    try {
     if (pokemonText.value) {
 const xhr = new XMLHttpRequest();
 const url = `https://pokeapi.co/api/v2/pokemon/`;
 xhr.responseType = 'json';
-
+pokemonText.value = '';
 xhr.onreadystatechange = () => {
+    weight.innerHTML = '';
     if (xhr.readyState === XMLHttpRequest.DONE) {
+        try {
+        loading.innerText = '';
         const pokemon = xhr.response;
         let weightInPounds = pokemon.weight / 4.54;
         weightInPounds = weightInPounds.toFixed(2);
            weight.innerHTML = `${pokemon.name}: weight: ${weightInPounds}lbs`
+        } catch (e) {
+            weight.innerHTML = 'Please choose a valid Pokemon';
+            console.log(e);
+        }
         }
     }
 xhr.open('GET',url + pokemonName);
 xhr.send();
-    } else {
+    } 
+    } catch (e) {
         weight.innerHTML = 'Please choose a valid Pokemon';
+        console.log(e);
     }
 }
 
