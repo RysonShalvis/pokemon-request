b159d229b11616f5993b2d4712bfc8e26b7d4926
Ryson Shalvis <rysonshalvis@Rysons-MacBook-Pro.local>
2020-09-15 21:44:37 -0600

    add any pokemon

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 const button = document.querySelector('button');
-const weight = document.querySelector('#pokemon-weight')
-
+const weight = document.querySelector('#pokemon-weight');
+const pokemonText = document.querySelector('#pokemon-text');
+function pokemonRequest () {
+    const pokemonName = pokemonText.value;
+    if (pokemonText.value) {
 const xhr = new XMLHttpRequest();
-const url = 'https://pokeapi.co/api/v2/pokemon/eevee';
+const url = `https://pokeapi.co/api/v2/pokemon/`;
 xhr.responseType = 'json';
 
 xhr.onreadystatechange = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
         const pokemon = xhr.response;
-        function showWeight() {
+        
            weight.innerHTML = `${pokemon.name}: weight: ${pokemon.weight}`
         }
-
-        button.addEventListener('click', showWeight)
+    }
+xhr.open('GET',url + pokemonName);
+xhr.send();
     }
 }
 
-xhr.open('GET',url);
-xhr.send();
+button.addEventListener('click',pokemonRequest);
