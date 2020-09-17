b83feadd49cc4d74b9981362d0c5442cd9377050
Ryson Shalvis <rysonshalvis@Rysons-MacBook-Pro.local>
2020-09-16 21:34:26 -0600

    add image to pokemon choice

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const weight = document.querySelector('#pokemon-weight');
 const pokemonText = document.querySelector('#pokemon-text');
 const loadContainer = document.querySelector('.load-container');
 const anyPokemonInfo = document.querySelector('#pokemon-anything');
+const pokemonImage = document.querySelector('#pokemon-image');
 function pokemonRequest () {
-    
+    pokemonImage.src = 'https://img.pokemondb.net/artwork/large/'
     let loading = document.createElement('p');
     loading.innerHTML = 'Loading';
     loadContainer.appendChild(loading);
@@ -14,21 +15,22 @@ function pokemonRequest () {
 const xhr = new XMLHttpRequest();
 const url = `https://pokeapi.co/api/v2/pokemon/`;
 xhr.responseType = 'json';
-pokemonText.value = '';
 xhr.onreadystatechange = () => {
     weight.innerHTML = '';
     if (xhr.readyState === XMLHttpRequest.DONE) {
         try {
-        loading.innerText = '';
-        const pokemon = xhr.response;
-        let weightInPounds = pokemon.weight / 4.54;
-        weightInPounds = weightInPounds.toFixed(2);
-           weight.innerHTML = `${pokemon.name}: weight: ${weightInPounds}lbs`;
-           anyPokemonInfo.innerHTML = pokemon.height;
+            loading.innerText = '';
+            const pokemon = xhr.response;
+            let weightInPounds = pokemon.weight / 4.54;
+            weightInPounds = weightInPounds.toFixed(2);
+            weight.innerHTML = `${pokemon.name}: weight: ${weightInPounds}lbs`;
+            anyPokemonInfo.innerHTML = pokemon.height;
+            pokemonImage.src =  pokemonImage.src + pokemonText.value + '.jpg';
         } catch (e) {
             weight.innerHTML = 'Please choose a valid Pokemon';
             console.log(e);
         }
+        pokemonText.value = '';
         }
     }
 xhr.open('GET',url + pokemonName);
@@ -40,4 +42,5 @@ xhr.send();
     }
 }
 
+console.log(pokemonImage)
 button.addEventListener('click',pokemonRequest);
